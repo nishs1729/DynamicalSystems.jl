5c8add10e9e904c04a78c2a0833c4842108d747e
Documenter.jl <documenter@juliadocs.github.io>
2023-12-23 08:09:40 +0000

    build based on 5a096eb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DynamicalSystems","category":"page"},{"location":"#DynamicalSystems","page":"Home","title":"DynamicalSystems","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DynamicalSystems.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DynamicalSystems]","category":"page"}]
+}
